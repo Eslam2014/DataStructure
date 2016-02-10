f05f06af238ab16a1addb010eea40eef8a577a2c
eslam <abkarino.em2012@gmail.com>
2016-02-10 03:51:40 +0200

    update linkedlist implement list functions

--- DataStucture/src/linkedlist/Linkedlist.java
+++ DataStucture/src/linkedlist/Linkedlist.java
@@ -1,172 +1,386 @@
 package linkedlist;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-public class Linkedlist<T> {
-	protected Node<T> head;
-	protected Node<T> tail;
-	protected int size;
-	
-public Linkedlist() {
-		// TODO Auto-generated constructor stub
+public class Linkedlist<T> implements List<T> {
+
+	static class Node<T> {
+		private T value;
+		private Node<T> next;
+
+		public Node(T value) {
+			this.value = value;
+			this.next = null;
+		}
+
+		public T getValue() {
+			return value;
+		}
+
+		public Node<T> getNext() {
+			return next;
+		}
+
+		public void setNext(Node<T> next) {
+			this.next = next;
+		}
+
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
+
+	public Linkedlist() {
 		head = null;
 		tail = null;
 		size = 0;
-
 	}
 
-public void append(T value) {
-	checkValue(value, "not allow to insert null value");	
+	@Override
+	public boolean add(T value) {
+		checkValue(value, "not allow to insert null value");
+
 		if (head == null) {
 			head = tail = new Node<T>(value);
 			size++;
-		} else {
-			Node<T> tmp = new Node<T>(value);
-			tail.setNext(tmp);
-			tail = tmp;
-			size++;
+			return true;
 		}
+		Node<T> tmp = new Node<T>(value);
+		tail.next = tmp;
+		tail = tmp;
+		size++;
+		return true;
 	}
 
-public int findIndexOf(T value) {
-	checkValue(value, "can't search for null value");
+	@Override
+	public void add(int index, T element) {
+		if (index != size)
+			checkIndex(index);
+		checkValue(element, "not allow to insert null value");
+
+		Node<T> node = new Node<T>(element);
+		if (index == 0) {
+			node.next = head;
+			head = node;
+
+		}
+		Node<T> tmp = getPrevNode(index);
+		node.next = tmp.next;
+		tmp.next = node;
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (size == 0)
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+
+		if (o == null || checkObjectType(o))
+			return false;
+
+		return indexOf(o) != -1;
+	}
+
+	private boolean checkObjectType(Object o) {
+		if (o instanceof Linkedlist<?>) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		if (o == null || !checkObjectType(o))
+			return -1;
+
 		Node<T> tmp = head;
 		int counter = 0;
 		while (tmp != null) {
-			counter++;
-			if (tmp.getValue().equals(value)) {
-				return counter-1;
+			if (tmp.value.equals(o)) {
+				return counter;
 			}
-			tmp = tmp.getNext();
+			counter++;
+			tmp = tmp.next;
 		}
 		return -1;
 	}
 
-public T findValueOf(int index) {
-	checkIndex(index);
+	@Override
+	public Iterator<T> iterator() {
+
+		Iterator<T> it = new Iterator<T>() {
 			Node<T> tmp = head;
-			int counter = 0;
-			while (counter <= index) {
-				if (counter == index) {
-					return tmp.getValue();
-				} else {
-					tmp = tmp.getNext();
-					counter++;
+
+			@Override
+			public boolean hasNext() {
+				if (tmp != null)
+					return true;
+				return false;
+			}
+
+			@Override
+			public T next() {
+				if (tmp != null) {
+					T value = tmp.value;
+					tmp = tmp.next;
+					return value;
 				}
-			}	
+				return null;
+			}
+		};
+		return it;
+	}
+
+	@Override
+	public Object[] toArray() {
+		Node<T> tmp = head;
+		Object[] objectLinkedlist = new Object[size];
+		int index = 0;
+		while (tmp != null) {
+			objectLinkedlist[index] = tmp;
+			tmp = tmp.next;
+			index++;
+		}
+		return objectLinkedlist;
+
+	}
+
+	@SuppressWarnings("hiding")
+	@Override
+	public <T> T[] toArray(T[] a) {
+
+		T[] arrayLinkedlist = a.length < size ? Arrays.copyOf(a, size) : a;
+
+		@SuppressWarnings("unchecked")
+		Node<T> tmp = (Node<T>) head;
+		int index = 0;
+		while (tmp != null) {
+			arrayLinkedlist[index] = tmp.value;
+			tmp = tmp.next;
+			index++;
+		}
+		return arrayLinkedlist;
+	}
+
+	@Override
+	public T get(int index) {
+
+		checkIndex(index);
+		Node<T> tmp = head;
+
+		for (int counter = 0; counter <= index; counter++) {
+			tmp = tmp.next;
+		}
+		return tmp.value;
+
+	}
+
+	@Override
+	public T set(int index, T element) {
+
+		if (index != size)
+			checkIndex(index);
+		checkValue(element, "not allow to insert null value");
+
+		Node<T> node = new Node<T>(element);
+		if (index == 0) {
+			head = node;
+			return null;
+		}
+
+		Node<T> tmp = getPrevNode(index);
+
+		node.next = tmp.next.next;
+		tmp.next = node;
+		return tmp.value;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if (o != null || head == null || checkObjectType(o))
+			return false;
+
+		@SuppressWarnings("unchecked")
+		T value = (T) o;
+		if (head.value.equals(value)) {
+			if (size == 1) {
+				head = tail = null;
+			}
+			head = head.next;
+			size--;
+			return true;
+		}
+
+		Node<T> tmp = head;
+		while (tmp.next != null) {
+			if (tmp.next.value.equals(value)) {
+				Node<T> preTmp = tmp.next;
+				if (preTmp.next == null)
+					tail = tmp;
+				tmp.next = preTmp.next;
+				preTmp = null;
+				size--;
+				return true;
+
+			} else
+				tmp = tmp.next;
+		}
+		return false;
+
+	}
+
+	@Override
+	public T remove(int index) {
+		checkIndex(index);
+		T value;
+		if (index == 0) {
+			value = head.value;
+			head = tail = null;
+			size--;
+			return value;
+		}
+
+		Node<T> tmp = getPrevNode(index);
+		Node<T> dTmp = tmp.next;
+		value = dTmp.value;
+		if (dTmp.next == null)
+			tail = tmp;
+		tmp.next = dTmp.next;
+		dTmp = null;
+		size--;
+		return value;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		if (c == null) {
+			return false;
+		}
+		for (T t : c) {
+			add(t);
+		}
+		return true;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends T> c) {
+		if (index != size) {
+			checkIndex(index);
+		}
+		if (c == null) {
+			return false;
+		}
+
+		int count = index;
+		for (T t : c) {
+			add(count, t);
+			count++;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		if (c == null) {
+			return false;
+		}
+
+		boolean result = true;
+		for (Object t : c) {
+			result &= remove(t);
+		}
+		return result;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		Node<T> tmp=head;
+		while(tmp!=null){
+			head=tmp.next;
+			tmp=null;
+			tmp=head;
+		}
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ListIterator<T> listIterator() {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
-public void insert(T value,int index){
-	checkIndex(index);
-	checkValue(value, "not allow to insert null value");
-	Node<T> node=new Node<T>(value);
-	
-		Node<T> tmp = head;
-		if(index==0){
-			
-			node.setNext(head);
-			head=node;
-		}
-		else if(index == size-1){
-			node.setNext(tail);
-			tail=node;
-		}
-		else
-		{
-		int counter = 1;
-		while (counter <= index) {
-			if (counter == index-1) {
-				node.setNext(tmp.getNext());
-				tmp.setNext(node);
-					break;
-			} 
-			else {
-				tmp = tmp.getNext();
-				counter++;
-				}
-		}
-		
-		}
-		
-	
-}
 
-public void delete(int index){
-	checkIndex(index);
-		Node<T> tmp = head;
-		int counter = 0;
-		while (counter <= index) {
-			if (counter == index-1) {
-				Node<T> dTmp=tmp.getNext();
-				if(tmp.getNext().getNext()==null)
-					tail=tmp;
-				tmp.setNext(tmp.getNext().getNext());
-				dTmp=null;
-				size--;
-	           break;			
-			} else {
-				tmp = tmp.getNext();
-				counter++;
-			}
-		}
-		
-}
-public void deleteValue(T value){
-	checkValue(value, "there are no null value in linked list");
-   
-	Node<T> tmp=head;
-	if(head.getValue().equals(value)){
-		head=head.getNext();
-		tmp=null;
-		size--;
+	@Override
+	public ListIterator<T> listIterator(int index) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-	else{
-	while(tmp.getNext()!=null){
-		
-		if(tmp.getNext().getValue().equals(value)){
-			Node<T> preTmp=tmp.getNext();
-			if(tmp.getNext().getNext()==null)
-				tail=tmp;
-			tmp.setNext(tmp.getNext().getNext());
-			preTmp=null;
-			size--;
-			break;
-		}
-		else
-			tmp=tmp.getNext();
-	}
-	}
-}
 
-private void checkValue(T value,String message){
-	if(value == null){
-		   throw new IllegalArgumentException(message);
-	   }
-	
-}
-private void checkIndex(int index) {
-	if(index < 0 ||index >= size){
-		throw new IndexOutOfBoundsException("index "+index+" out of rang");
+	@Override
+	public List<T> subList(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return null;
 	}
-}
-public int size() {
+
+	private void checkValue(T value, String message) {
+		if (value == null) {
+			throw new IllegalArgumentException(message);
+		}
+
+	}
+
+	public int size() {
 		return size;
 	}
 
-	public Node<T> getHead() {
-		return head;
-	}
-
-	public Node<T> getTail() {
-		return tail;
-	}
-
-	public void display(){
-		Node<T> tmp=head;
-		while (tmp!=null) {
-			System.out.print(tmp.getValue()+" -> ");
-			tmp=tmp.getNext();
+	@Override
+	public String toString() {
+		StringBuilder linkedlist = new StringBuilder();
+		Node<T> tmp = head;
+		while (tmp != null) {
+			linkedlist.append(tmp.value + " -> ");
+			tmp = tmp.next;
 		}
-		System.out.println(" ");
+		return linkedlist.toString();
+	}
+
+	private void checkIndex(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("index " + index + " out of range");
+		}
+	}
+
+	
+	private Node<T> getPrevNode(int index) {
+		Node<T> tmp = head;
+		for (int counter = 0; counter < index; counter++) {
+			tmp = tmp.next;
+		}
+		return tmp;
 	}
 }
